b90d805ba1d86dc1fc4d616e29c9368132869c2e
vinayNrwal <vinay06@metafyapps.com>
2025-03-13 18:49:32 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import CryptoTable from "./components/CryptoTable";
-// import AIInsights from "./components/AIInsights";
 import PortfolioTracker from "./components/PortfolioTracker";
 import CryptoChart from "./components/CryptoChart";
 
@@ -11,7 +10,6 @@ function App() {
       <Navbar />
       <HeroSection />
       <CryptoTable />
-      {/* <AIInsights /> */}
       <PortfolioTracker />
       <CryptoChart />
     </div>
